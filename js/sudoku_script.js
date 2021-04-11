7cc37d417cc7d35928c31119fceedc956c33ec99
Darius Feher <darius.feher@student.manchester.ac.uk>
2021-04-11 14:45:34 +0300

    Improve the game architecture

--- js/sudoku_script.js
+++ js/sudoku_script.js
@@ -1,83 +1,72 @@
-function beginnerGame() {
-	newGame(15);
-}
-
-function intermediateGame() {
-	newGame(45);
-}
-
-function expertGame() {
-	newGame(60);
-}
-
 function newGame(no_digits_to_be_removed) {
-	var board = document.getElementById('board');
-	if (board) {
-		var parentBoard = board.parentElement;
-		parentBoard.removeChild(board);
+	var boardElement = document.getElementById('board');
+	if (boardElement) {
+		var parentBoard = boardElement.parentElement;
+		parentBoard.removeChild(boardElement);
 	}
 
-	this.board = new Array(9);
+	var winMessageHeader = document.getElementById('winMessageHeader');
+	if (winMessageHeader) {
+		var parentHeader = winMessageHeader.parentElement;
+		parentHeader.removeChild(winMessageHeader);
+	}
 
-	window.addEventListener('keydown',function() {fillCell(event)});
+	board = new Array(9);
+	solution_board = board;
+	board_after_remove = board;
 
-	this.current_row = -1;
-	this.current_col = -1;
+	current_row = [-1];
+	current_col = [-1];
 
-	createBoard();
+	window.addEventListener('keydown',function() {fillCell(event, current_row, current_col, board, solution_board, board_after_remove)});
 
-	fillDiagonalBoxes();
+	createBoard(board, current_row, current_col);
 
-	fillRemaining(0, 3);
+	fillDiagonalBoxes(board);
 
-	this.solution_board = getBoardCopy();
+	fillRemaining(0, 3, board);
 
-	removeKDigits(no_digits_to_be_removed);
+	solution_board = getBoardCopy(board);
 
-	this.board_after_remove = getBoardCopy();
+	removeKDigits(no_digits_to_be_removed, board);
 
-	updateBoardView();
+	board_after_remove = getBoardCopy(board);
+
+	updateBoardView(board);
 }
 
 function getCellId(row, col) {
 	return row.toString() + "," + col.toString();
 }
 
-function selectCell(event, cell) {
+function selectCell(event, cell, current_row, current_col) {
 	var res = cell.id.split(",");
 	var x = parseInt(res[0])
 	var y = parseInt(res[1]);
-	
-	if (this.current_row != -1 && this.current_col != -1) {
-		var last_cell = document.getElementById(getCellId(this.current_row, this.current_col));
-		last_cell.style.backgroundColor = "white";
-		shadeUnshadeRow(this.current_row, "unshade");
-		shadeUnshadeCol(this.current_col, "unshade");
-		shadeUnshadeBox(this.current_row, this.current_col, "unshade");
+	console.log(current_row, current_col);
+	if (current_row != -1 && current_col != -1) {
+		shadeUnshadeColRowBox(current_row, current_col, "unshade")
 	}
-	
-	shadeUnshadeRow(x, "shade");
-	shadeUnshadeCol(y, "shade");
-	shadeUnshadeBox(x, y, "shade");
-	cell.style.backgroundColor = "DarkGreen";
-	this.current_row = x;
-	this.current_col = y;
+	shadeUnshadeColRowBox(x, y, "shade")
+	cell.className = "current_selected_cell";
+	current_row[0] = x;
+	current_col[0] = y;
 }
 
-function createBoard() {
+function createBoard(board, current_row, current_col) {
 	var table = document.createElement('table');
 	table.className = "boardContainer";
 	table.id = "board";
 	for (var i = 0; i < 9; i++) {
-		this.board[i] = new Array(9);
+		board[i] = new Array(9);
 	    var tr = document.createElement('tr');
 		for (var j = 0; j < 9; j++) {
-			this.board[i][j] = 0;
+			board[i][j] = 0;
 		    var td = document.createElement('td');
 		    var cell = document.createElement("BUTTON");
 		    cell.id = i.toString() + "," + j.toString();
-		    cell.className = "table_but";
-		    cell.onmousedown = function() {selectCell(event, this)};
+		    cell.className = "empty_cell";
+		    cell.onmousedown = function() {selectCell(event, this, current_row, current_col)};
 		    td.appendChild(cell);
 		    tr.appendChild(td);
 		}
@@ -87,119 +76,125 @@ function createBoard() {
 	document.getElementById("table").appendChild(table);
 }
 
-function fillCell(event) {
-	if (this.current_row != -1 && this.current_col != -1) {
-		var i = this.current_row;
-		var j = this.current_col;
-		var cell = document.getElementById(getCellId(i, j));
-		if (cell.disabled == false) {
-			if (this.board_after_remove[i][j] == 0) {
-				if (event.key >= 1 && event.key <= 9) {
-					cell.innerHTML = event.key;
-					this.board[i][j] = parseInt(event.key);
-					updateSafeUnsafeValues();
-					if (checkWin()) {
-						alert("Congratulations, you won!");
-						disableCells();
-					}
-				} else if (event.key == "Backspace") {
-					cell.innerHTML = "";
-					this.board[i][j] = 0;
+function fillCell(event, current_row, current_col, board, solution_board, board_after_remove) {
+	if (current_row != -1 && current_col != -1) { // Check if the user selected a cell before pressing a key
+		var cell = document.getElementById(getCellId(current_row, current_col));
+		updateContentAndColor(board, cell, current_row, current_col, event, board_after_remove)
+	}
+}
+
+function updateContentAndColor(board, cell, i, j, event, board_after_remove) {
+	if (cell.disabled == false && board_after_remove[i][j] == 0) {
+		if (event.key >= 1 && event.key <= 9) {
+			handleValidDigit(board, cell, i, j, parseInt(event.key));
+		} else if (event.key == "Backspace") {
+			handleBackSpace(cell, i, j, board, board_after_remove);
+		}
+	}
+}
+
+function handleValidDigit(board, cell, i, j, digit) {
+	cell.innerHTML = event.key;
+	if (checkIfSafe(i, j, parseInt(event.key)) == false) {
+		cell.style.color = "red";
+	} else {
+		cell.style.color = "DarkBlue";
+	}
+	board[i][j] = digit;
+	updateSafeUnsafeValues(board, board_after_remove);
+
+	if (checkWin(board, solution_board)) {
+		var winMessageDiv = document.getElementById("winMessage");
+		var winMessageHeader = document.createElement("h1");
+		winMessageHeader.id = "winMessageHeader"
+		winMessageHeader.innerHTML = "Congratulations, you won!"
+		winMessageDiv.appendChild(winMessageHeader);
+		disableCells();
+	}
+}
+
+function handleBackSpace(cell, i, j, board, board_after_remove) {
+	cell.innerHTML = "";
+	board[i][j] = 0;
+	updateSafeUnsafeValues(board, board_after_remove);
+}
+
+
+function shadeUnshadeColRowBox(row, col, action) {
+	if (action == "shade") {
+		// Shade BOX
+		var rowSt = Math.floor(row / 3) * 3;
+		var colSt = Math.floor(col / 3) * 3;
+		for (var i = 0; i < 3; i++) {
+			for (var j = 0; j < 3; j++) {
+				var cell = document.getElementById(getCellId(rowSt + i, colSt + j));
+				cell.className = "cell_shaded"
+			}
+		}
+		// Shade ROW
+		for (var j = 0; j < 9; j++) {
+			var cell = document.getElementById(getCellId(row, j));
+			cell.className = "cell_shaded"
+		}
+		// Shade COL
+		for (var i = 0; i < 9; i++) {
+			var cell = document.getElementById(getCellId(i, col));
+			cell.className = "cell_shaded"
+		}
+
+	} else if (action == "unshade") {
+		for (var i = 0; i < 9; i++) {
+			for (var j = 0; j < 9; j++) {
+				var cell = document.getElementById(getCellId(i, j));
+				if (cell.className == "cell_shaded" || cell.className == "current_selected_cell") {
+					cell.className = "empty_cell"
 				}
 			}
 		}
 	}
 }
 
-function shadeUnshadeBox(row, col, action) {
-	var rowSt = Math.floor(row / 3) * 3;
-	var colSt = Math.floor(col / 3) * 3;
-	for (var i = 0; i < 3; i++) {
-		for (var j = 0; j < 3; j++) {
-			var cell = document.getElementById(getCellId(rowSt + i, colSt + j));
-			if (action == "shade") {
-				cell.style.backgroundColor = "DarkSeaGreen";
-			} else if (action == "unshade") {
-				cell.style.backgroundColor = "white";
-			}
-		}
-	}
-}
-
-
-function shadeUnshadeRow(row, action) {
-	for (var j = 0; j < 9; j++) {
-		var cell = document.getElementById(getCellId(row, j));
-		if (action == "shade") {
-			cell.style.backgroundColor = "DarkSeaGreen";
-		} else if (action == "unshade") {
-			cell.style.backgroundColor = "white";
-		}
-	}
-}
-
-function shadeUnshadeCol(col, action) {
-	for (var i = 0; i < 9; i++) {
-		var cell = document.getElementById(getCellId(i, col));
-		if (action == "shade") {
-			cell.style.backgroundColor = "DarkSeaGreen";
-		} else if (action == "unshade") {
-			cell.style.backgroundColor = "white";
-		}
-	}
-}
-
-function fillDiagonalBoxes() {
+function fillDiagonalBoxes(board) {
     for (var i = 0; i < 9; i += 3)
-        fillBox(i, i);
-}
-  
-function unUsedInBox(rowStart, colStart, digit) {
-    for (var i = 0; i < 3; i++) {
-        for (var j = 0; j<3; j++) {
-            if (this.board[rowStart + i][colStart + j] == digit) {
-                return false;
-            }
-        }
-    }
-    return true;
+        fillBox(i, i, board);
 }
 
-function fillBox(row, col) {
+function fillBox(row, col, board) {
     var digit;
     for (var i = 0; i < 3; i++) {
         for (var j = 0; j < 3; j++) {
         	digit = 1 + Math.floor(Math.random() * 9);
-            while (!unUsedInBox(row, col, digit)) {
+            while (!checkIfSafe(row, col, digit)) {
             	digit = 1 + Math.floor(Math.random() * 9);
             }
-            this.board[row + i][col + j] = digit;
+            board[row + i][col + j] = digit;
         }
     }
 }
 
-  
-function CheckIfSafe(i, j, digit) {
-    return (unUsedInRow(i, digit) &&
-            unUsedInCol(j, digit) &&
-            unUsedInBox(i - i % 3, j - j % 3, digit));
-}
-  
-function unUsedInRow(i, digit) {
-    for (var j = 0; j < 9; j++)
-       if (this.board[i][j] == digit)
+function checkIfSafe(row, col, digit) {
+	// Check if used in ROW
+	for (var j = 0; j < 9; j++)
+       if (board[row][j] == digit)
             return false;
-    return true;
-}
-  
-function unUsedInCol(j, digit) {
+
+    // Check if used in COL
     for (var i = 0; i<9; i++)
-        if (this.board[i][j] == digit)
+        if (board[i][col] == digit)
             return false;
-    return true;
+
+    // Check if used in BOX
+    for (var i = 0; i < 3; i++) {
+        for (var j = 0; j<3; j++) {
+            if (board[row - row % 3 + i][col - col % 3 + j] == digit) {
+                return false;
+            }
+        }
+    }
+    return true
 }
-  
-function fillRemaining(i, j) {
+
+function fillRemaining(i, j, board) {
     if (j >= 9 && i < 8) {
         i++;
         j = 0;
@@ -228,73 +223,75 @@ function fillRemaining(i, j) {
     }
 
     for (var digit = 1; digit <= 9; digit++) {
-        if (CheckIfSafe(i, j, digit)) {
-            this.board[i][j] = digit;
-            if (fillRemaining(i, j + 1)) {
+        if (checkIfSafe(i, j, digit)) {
+            board[i][j] = digit;
+            if (fillRemaining(i, j + 1, board)) {
                 return true;
             }
-            this.board[i][j] = 0;
+            board[i][j] = 0;
         }
     }
     return false;
 }
 
-function removeKDigits(no_digits_to_be_removed) {
+function removeKDigits(no_digits_to_be_removed, board) {
     var count = no_digits_to_be_removed;
     while (count != 0) {
         var i = Math.floor(Math.random() * 9);
         var j = Math.floor(Math.random() * 9);
 
-        if (this.board[i][j] != 0) {
+        if (board[i][j] != 0) {
             count--;
-            this.board[i][j] = 0;
+            board[i][j] = 0;
         }
     }
 }
 
-function getBoardCopy() {
+function getBoardCopy(board) {
 	var board_copy = new Array(9);
 
 	for (var i = 0; i < 9; i++) {
 		board_copy[i] = new Array(9);
 		for (var j = 0; j < 9; j++) {
-			board_copy[i][j] = this.board[i][j];
+			board_copy[i][j] = board[i][j];
 		}
 	}
 	return board_copy;
 }
 
-function updateBoardView() {
+function updateBoardView(board) {
 	for (var i = 0; i < 9; i++) {
 		for (var j = 0; j < 9; j++) {
 		    var cell = document.getElementById(i.toString() + "," + j.toString());
-		    if (this.board[i][j]) {
-		    	cell.innerHTML = this.board[i][j];
+		    if (board[i][j]) {
+		    	cell.innerHTML = board[i][j];
 		    }
 		}
 	}
 }
 
-function updateSafeUnsafeValues() {
+function updateSafeUnsafeValues(board, board_after_remove) {
 	for (var i = 0; i < 9; i++) {
 		for (var j = 0; j < 9; j++) {
-		    var cell = document.getElementById(i.toString() + "," + j.toString());
-		    var aux = this.board[i][j];
-		    this.board[i][j] = 0;
-		    if (CheckIfSafe(i, j, aux)) {
-		    	cell.style.color = "DarkBlue";
-		    } else {
-		    	cell.style.color = "red";
-		    }
-		    this.board[i][j] = aux;
+			if (board_after_remove[i][j] == 0) {
+			    var cell = document.getElementById(i.toString() + "," + j.toString());
+			    var aux = board[i][j];
+			    board[i][j] = 0;
+			    if (checkIfSafe(i, j, aux)) {
+			    	cell.style.color = "DarkBlue";
+			    } else {
+			    	cell.style.color = "red";
+			    }
+			    board[i][j] = aux;
+			}
 		}
 	}
 }
 
-function checkWin() {
+function checkWin(board, solution_board) {
 	for (var i = 0; i < 9; i++) {
 		for (var j = 0; j < 9; j++) {
-		    if (this.board[i][j] != this.solution_board[i][j]) {
+		    if (board[i][j] != solution_board[i][j]) {
 		    	return false;
 		    }
 		}
